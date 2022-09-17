20afa94fe11b7c606e61f79f959db23d60c1833d
s1936914629 <1936914629@qq.com>
2022-09-17 20:53:39 +0800

    类别管理__ID查询__控制层

--- mall-product/mall-product-webapi/src/main/java/cn/org/sqx/mall/product/webapi/controller/CategoryController.java
+++ mall-product/mall-product-webapi/src/main/java/cn/org/sqx/mall/product/webapi/controller/CategoryController.java
@@ -2,14 +2,12 @@ package cn.org.sqx.mall.product.webapi.controller;
 
 import cn.org.sqx.mall.common.web.JsonResult;
 import cn.org.sqx.mall.pojo.dto.CategoryAddNewDTO;
+import cn.org.sqx.mall.pojo.vo.CategoryDetailsVO;
 import cn.org.sqx.mall.pojo.vo.CategorySimpleListItemVO;
 import cn.org.sqx.mall.product.service.ICategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,12 +29,18 @@ public class CategoryController {
         return JsonResult.ok();
     }
 
+    @GetMapping("/{id}")
+    public JsonResult<CategoryDetailsVO> getDetailsById(@PathVariable Long id){
+        CategoryDetailsVO category = iCategoryService.getDetailsById(id);
+        return JsonResult.ok(category);
+
+    }
+
     @GetMapping("/list-by-parent")
     public JsonResult<List<CategorySimpleListItemVO>> listByParentId(Long parentId) {
         // 调用service并将结果封装到JsonResult中
         List<CategorySimpleListItemVO> list = iCategoryService.listByParentId(parentId);
         return JsonResult.ok(list);
-
     }
 
 }

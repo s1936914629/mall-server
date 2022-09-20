0c5d2362b7ee7f0e51485982223a05ef5cace024
s1936914629 <1936914629@qq.com>
2022-09-20 21:10:04 +0800

    加入redis

---
+++ mall-product/mall-product-webapi/src/main/java/cn/org/sqx/mall/product/webapi/config/RedisConfiguration.java
@@ -0,0 +1,25 @@
+package cn.org.sqx.mall.product.webapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+import java.io.Serializable;
+
+/**
+ * @auther: sqx
+ * @Date: 2022-09-17
+ */
+@Configuration
+public class RedisConfiguration {
+    @Bean
+    public RedisTemplate<String, Serializable> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Serializable> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(RedisSerializer.string());
+        redisTemplate.setValueSerializer(RedisSerializer.json());
+        return redisTemplate;
+    }
+}

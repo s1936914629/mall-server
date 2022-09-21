68eee0a03e1cb0af14ad127b436775cff6fe6ba4
s1936914629 <1936914629@qq.com>
2022-09-21 18:36:17 +0800

    增加管理员

---
+++ mall-passport/src/main/java/cn/org/sqx/mall/passport/MallPassportApplication.java
@@ -0,0 +1,15 @@
+package cn.org.sqx.mall.passport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @auther: sqx
+ * @Date: 2022-09-21
+ */
+@SpringBootApplication
+public class MallPassportApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallPassportApplication.class, args);
+    }
+}
